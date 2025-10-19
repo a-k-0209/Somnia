f9da52c2a9717b7d3bd542d5769e49184f42561d
a-k-0209 <anikakarampuri04@.com>
2025-10-19 17:41:53 +0530

    initial files

--- utils/call_model.py
+++ utils/call_model.py
@@ -14,16 +14,3 @@ class CallModel():
             temperature=temperature,
             api_key=SecretStr(api_key)
         )
-
-
-    # def call_model(self, max_tokens=3000, temperature=0.1):
-    #     api_key = "AIzaSyCUPjby__GDwPYklqWc1wei3FbAXZsOwGA"  # Or use os.getenv("GOOGLE_API_KEY")
-    #     llm = ChatGoogleGenerativeAI(
-    #         model="gemini-2.5-flash",
-    #         google_api_key=api_key,
-    #         temperature=temperature,
-    #         max_output_tokens=max_tokens,
-    #     )
-    #     # resp = llm.invoke(prompt)
-    #     return llm
-    # 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
